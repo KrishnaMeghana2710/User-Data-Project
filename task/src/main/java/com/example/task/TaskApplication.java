7453fcd0884c3cf9409af2a6fccaa725d9ec89fe
meghana <krishnameghana2710@gmail.com>
2023-07-29 21:22:20 +0530

    added dev and prod profiles

--- task/src/main/java/com/example/task/TaskApplication.java
+++ task/src/main/java/com/example/task/TaskApplication.java
@@ -3,7 +3,6 @@ package com.example.task;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 
 @SpringBootApplication
